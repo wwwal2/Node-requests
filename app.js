48560fc36354955200abbefb70d38e77579de8b3
wwwal2 <smalex9@gmail.com>
2018-07-09 22:02:37 +0300

    setting ip request

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const helpFromNpm = require('request');
 const helpFromNpm2 = require('yargs');
 
-const argv = helpFromNpm2;
+const parsedYargs = helpFromNpm2
+    .option('i', {
+        alias: 'ip',
+        demandOption: true,
+        describe: 'Ip to show a location ***.***.***.***',
+        string: true
+    })
+    .help().alias('h', 'help')
+    .argv;
+
+// // UNNESSESARY BLOCK FOR URL ENCODING
+// const encodeUrl =  encodeURIComponent(parsedYargs.address);
+// const decodeUrl =  decodeURIComponent(parsedYargs.address);
+// console.log(`decoded url is ${decodeUrl}`);
+// console.log(`emcoded url is ${encodeUrl}`);
+
+let userIpInput = parsedYargs.ip;
+
 
 helpFromNpm({
-    url: 'https://jsonplaceholder.typicode.com/users',
+    url: `http://api.ipstack.com/${userIpInput}?access_key=a690d859ca69cf463ec76784323e294b`,
     json: true
 }, (error, response, body) => {
-    console.log(body[1].address.street);
-    console.log(response);
+    console.log(body.continent_name, body.city);
+    console.log(`Latitude: ${body.latitude}, Longitude: ${body.longitude}`);
 });
+
+
+// Existing Ips "134.101.250.155", "114.101.250.125"
