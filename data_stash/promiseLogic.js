0c392d54935d5667576e33e8c7bec22c5989c41e
wwwal2 <smalex9@gmail.com>
2018-07-12 23:01:25 +0300

    chaining promise tutorial

--- data_stash/promiseLogic.js
+++ data_stash/promiseLogic.js
@@ -25,7 +25,7 @@ somePromise2.then((message) => {
     console.log('Fail: ', errorMessage)
 });
 
-// advanced promise
+// advanced promise + chaining promise
 const inputPromise = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -40,7 +40,35 @@ const inputPromise = (a, b) => {
 };
 
 inputPromise(4, 7).then((success) => {
-    console.log('The result is: ', success)
+    console.log('The result is: ', success);
+    return inputPromise(success, 25)        //chaining
 }, (error) => {
+    console.log(error);
+}).then((success2) => {                     //chaining
+    console.log('Chained result: ', success2);
+}, (error2) => {
+    console.log(error2);
+});
+
+//advanced promise + fixing chaining multiple errors
+const inputPromise2 = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Wrong arguments input2. Input numbers')
+            }
+
+        },2000);
+    });
+};
+
+inputPromise2(200, 75).then((success) => {
+    console.log('The result is: ', success);
+    return inputPromise(success, 30);
+}).then((success2) => {                     //chaining
+    console.log('Chained result: ', success2);
+}).catch((error) => {                       //complex error action for all errors
     console.log(error);
 });
